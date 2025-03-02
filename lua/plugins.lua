1a6f53d27c7de01ede18223720015724ba5448d6
daniyar <daniyarb331@gmail.com>
2025-03-03 01:56:24 +0700

    cmp&lspconfig was replaced by coc.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,20 +4,11 @@ return require("packer").startup(function(use)
     use "wbthomason/packer.nvim" -- Plugin manager
 
     use "nvim-treesitter/nvim-treesitter" -- Syntax highlighting
-    use "fatih/vim-go" -- Golang tools
-    use "neovim/nvim-lspconfig" -- Language Server Protocol
-
-    -- Auto-Completion
-    use {
-        "hrsh7th/nvim-cmp",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline"
-    }
-
     use "preservim/nerdtree" -- File manager
     use "junegunn/fzf"
     use "junegunn/fzf.vim"
     use "folke/tokyonight.nvim"
+    
+    -- Language Server Protocol & Completion
+    use { "neoclide/coc.nvim", branch = "release" }
 end)
