10d59ec2918fa9a06058df4483e38d10754b52d0
daniyar <daniyarb331@gmail.com>
2025-03-08 17:38:49 +0700

    config upd: lualine was added

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ vim.opt.scroll = 4
 vim.opt.signcolumn = "yes"
 
 -- user settings for screen scrolling
-vim.keymap.set("n", "<C-f>", "4jzz", { noremap = true, silent = true})
+vim.keymap.set("n", "<C-f>", "4jzz", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-d>", "4kzz", { noremap = true, silent = true })
 
 -- string numbering
@@ -17,13 +17,16 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 
+vim.g.NERDTreeDirArrows = 1
+
 -- disabling swap files creation
 vim.o.swapfile = false
 
 -- neovim plugins
 require("plugins")
-require("fzf")
-require("theme")
 require("coc-nvim")
+require("theme")
 require("nerdtree")
+require("fzf")
 require("treesitter")
+require("statusbar")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,10 +4,13 @@ return require("packer").startup(function(use)
     use "wbthomason/packer.nvim" -- Plugin manager
 
     use "nvim-treesitter/nvim-treesitter" -- Syntax highlighting
-    use "preservim/nerdtree" -- File manager
+    use "preservim/nerdtree" -- File-Manager
+    use "ryanoasis/vim-devicons" -- Dev-Icons
     use "junegunn/fzf"
     use "junegunn/fzf.vim"
-    use "folke/tokyonight.nvim"
+    use "catppuccin/nvim" -- Theme
+
+    use "nvim-lualine/lualine.nvim"
     
     -- Language Server Protocol & Completion
     use { "neoclide/coc.nvim", branch = "release" }
